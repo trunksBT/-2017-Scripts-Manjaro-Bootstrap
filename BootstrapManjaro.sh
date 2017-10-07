57a7736e4b686a5f2abeb274affbf5a6bcefa77f
trunksBT <209826@student.pwr.edu.pl>
2017-10-07 14:53:46 +0200

    Add bootstrap

---
+++ BootstrapManjaro.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+PATH_TO_VIMRC = 'https://github.com/ProjectsInCpp/-2016-Vim-.git'
+
+function set_ssh {
+}
+
+function update_software {
+	sudo pacman --sync --refresh
+	sudo pacman --sync --sysupgrade
+	reboot
+}
+
+function install_new_software{
+	sudo pacman --sync vim
+	sudo pacman --sync terminator
+	sudo pacman --sync git
+}
+
+function remove_artifacts{
+    rm -rf -- -2016-Vim-
+}
+
+function setup_vim{
+	git clone $PATH_TO_VIMRC
+	cd -- -2016-Vim-
+	rm -rf ~/.vimrc ~/.vim
+    cp --force .vimrc ~/.vimrc
+    cp --force --recursive .vim ~/.vim
+    cd ,,
+    remove_artifacts
+}
+
+# set_ssh 
+# install_new_software
+# install_new_software
+# setup_vim
+
